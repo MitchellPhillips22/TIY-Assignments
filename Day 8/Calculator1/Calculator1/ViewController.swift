e3b0afc3ddcd574bd446c7cea58ac04163383dbf
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-02-10 19:39:13 -0700

    Calculator Layout
    Calculator1 works; Calculator is in progress

---
+++ Day 8/Calculator1/Calculator1/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Calculator1
+//
+//  Created by Mitchell Phillips on 2/10/16.
+//  Copyright © 2016 Mitchell Phillips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var zeroOutlet: BorderButton!
+    @IBOutlet weak var oneOutlet: BorderButton!
+    @IBOutlet weak var twoOutlet: BorderButton!
+    @IBOutlet weak var threeOutlet: BorderButton!
+    @IBOutlet weak var fourOutlet: BorderButton!
+    @IBOutlet weak var fiveOutlet: BorderButton!
+    @IBOutlet weak var sixOutlet: BorderButton!
+    @IBOutlet weak var sevenOutlet: BorderButton!
+    @IBOutlet weak var eightOutlet: BorderButton!
+    @IBOutlet weak var nineOutlet: BorderButton!
+    @IBOutlet weak var decimalOutlet: BorderButton!
+    @IBOutlet weak var equalsOutlet: BorderButton!
+    @IBOutlet weak var parOutlet: BorderButton!
+    @IBOutlet weak var plusOutlet: BorderButton!
+    @IBOutlet weak var minusOutlet: BorderButton!
+    @IBOutlet weak var acOutlet: BorderButton!
+    @IBOutlet weak var divideOutlet: BorderButton!
+    @IBOutlet weak var timesOutlet: BorderButton!
+    @IBOutlet weak var deleteOutlet: BorderButton!
+    @IBOutlet weak var percentOutlet: BorderButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
